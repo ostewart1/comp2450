fda09a5c42d24a06ec233ac5d821626d75c7cb21
Olivia <ostewart1@harding.edu>
2026-08-24 11:01:42 -0500

    Floor 0 Done
    @HogRed

--- project/floor-00-starter/main.cpp
+++ project/floor-00-starter/main.cpp
@@ -21,7 +21,7 @@ int main() {
 
     // TODO Floor 0: replace this string with your own flavor line.
     // Anything a brave (or doomed) adventurer might mutter on descending.
-    const std::string flavor = "Change me, adventurer.";
+    const std::string flavor = "Ugh, I'm tired.";
 
     std::cout << "\nWelcome, " << hero_name << ".\n";
     std::cout << flavor << "\n\n";
